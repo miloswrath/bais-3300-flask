2562217508fe32c232e92ded85018b068b5fac87
zak <zakgilliam@gmail.com>
2026-04-06 13:30:20 -0500

    feat: oops! i did it all without screenshots

--- app.py
+++ app.py
@@ -1,13 +1,20 @@
 # module 10 - Flask Application
 # Zak Gilliam - 04/06/2026
 
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route("/")
-def hello():
-    return "Hello World!!"
+def home_page():
+    return render_template("index.html")
+
+
+@app.route("/about")
+def about_page():
+    return render_template("about.html")
+
 
 if __name__ == "__main__":
-      app.run(host="0.0.0.0", port=5002, debug=True)
+    app.run(host="0.0.0.0", port=5002, debug=True)
