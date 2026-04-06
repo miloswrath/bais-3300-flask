3008afd3b579ba28b5f26e40a39db003bb3b5f63
zak <zakgilliam@gmail.com>
2026-04-06 13:22:31 -0500

    feat: add app.py

---
+++ app.py
@@ -0,0 +1,13 @@
+# module 10 - Flask Application
+# Zak Gilliam - 04/06/2026
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return "Hello World!!"
+
+if __name__ == "__main__":
+      app.run(host="0.0.0.0", port=5002, debug=True)
